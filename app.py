94e8e02e597d24099ce8ce941125107a2aecf0a0
Andrew <blazersand2000@users.noreply.github.com>
2017-04-10 21:52:13 -0400

    Almost done getting our data to display with the ability to add and delete

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, redirect, request, url_for
 import sqlite3
 
-#conn = sqlite3.connect('db.sqlite3')
-#conn.row_factory = sqlite3.Row
-#c = conn.cursor()
+dbPath = 'db.sqlite3'
 
 app = Flask(__name__)
 
@@ -11,50 +9,103 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/missions")
+@app.route("/missions", methods=["GET", "POST"])
 def showMissions():
-    conn = sqlite3.connect('db.sqlite3')
+    conn = sqlite3.connect(dbPath)
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
     
-    c.execute("SELECT * FROM Mission")
+    if request.method == "POST":
+        for key in request.form:
+            if key.startswith('delete.'):
+                id = key.split('.')[1]
+                c.execute("DELETE FROM Mission WHERE mid = ?", (id,))
+                c.execute("DELETE FROM Crew WHERE mid = ?", (id,))
+                conn.commit()
+                return redirect(url_for("showMissions"))
+        if request.form["launchTime"]:
+            missionValues = (request.form["rid"], request.form["fid"], request.form["launchTime"], request.form["landTime"])
+            c.execute("INSERT INTO Mission (rid, fid, launchTime, landTime) VALUES (?, ?, ?, ?)", missionValues)
+            print(request.form.getlist("crewMembers"))
+            print(c.lastrowid)
+            astroValues = zip((c.lastrowid,) * len(request.form.getlist("crewMembers")), request.form.getlist("crewMembers"))
+            print(astroValues)
+            c.executemany("INSERT INTO Crew (mid, aid) VALUES (?, ?)", astroValues)
+    
+    #queries that populate table of missions
+    c.execute("SELECT M.mid, R.rname, F.name, M.launchTime, M.landTime, group_concat(A.firstName || ' ' || A.lastName, '<br/>') AS anames FROM Mission M, Rockets R, LaunchFacility F, Crew C, Astronauts A WHERE M.rid = R.rid AND M.fid = F.fid AND M.mid = C.mid AND C.aid = A.aid GROUP BY C.mid;")
     results = c.fetchall()
 
-    return render_template("missions.html", headers=("Mission ID","Rocket ID","Facility ID","Time of Launch"), rows=results)
+    #queries that populate rockets, facilities, and astronauts for the 'add new mission' form
+    c.execute("SELECT rid, rname || ' (Capacity: ' || cast(crewCapacity as text) || ')' FROM Rockets")
+    rockets = c.fetchall()
+    c.execute("SELECT fid, name FROM LaunchFacility")
+    facilities = c.fetchall()
+    c.execute("SELECT aid, firstName || ' ' || lastName FROM Astronauts")
+    astronauts = c.fetchall()
     
     conn.commit()
     conn.close()
+    
+    formInputs = [['rid', 'select', 'Rocket', rockets], ['fid', 'select', 'Launch Facility', facilities], ['crewMembers', 'selectmultiple', 'Crew Members', astronauts], ['launchTime', 'datetime', 'Launch Time', []], ['landTime', 'datetime', 'Land Time', []]]
 
-@app.route("/launch_facilities")
+    return render_template("missions.html", headers=("Mission ID","Rocket","Facility","Time of Launch", "Time of Landing", "Crew Members"), rows=results, inputs=formInputs)
+    
+@app.route("/launch_facilities", methods=["GET", "POST"])
 def showFacilities():
-    conn = sqlite3.connect('db.sqlite3')
+    conn = sqlite3.connect(dbPath)
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
+    
+    if request.method == "POST":
+        for key in request.form:
+            if key.startswith('delete.'):
+                id = key.split('.')[1]
+                c.execute("DELETE FROM LaunchFacility WHERE rid = ?", (id,))
+                conn.commit()
+                return redirect(url_for("showFacilities"))
+        if request.form["numPads"]:
+            userValues = (request.form["name"], request.form["numPads"], request.form["location"], request.form["lat"], request.form["long"])
+            c.execute("INSERT INTO LaunchFacility (name, numPads, location, lat, long) VALUES (?, ?, ?, ?, ?)", userValues)
     
     c.execute("SELECT * FROM LaunchFacility")
     results = c.fetchall()
-
-    return render_template("facilities.html", headers=("Facility ID","Location"), rows=results)
     
     conn.commit()
     conn.close()
+    
+    formInputs = [['name', 'text', 'Facility Name', []], ['numPads', 'number', 'Num. Launch Pads', []], ['location', 'text', 'Location', []], ['lat', 'text', 'Latitude', []], ['long', 'text', 'Longitude', []]]
+    
+    return render_template("facilities.html", headers=("Facility ID","Name","Num. Launch Pads","Location","Latitude","Longitude"), rows=results, inputs=formInputs)
+    
 
-@app.route("/rockets")
+@app.route("/rockets", methods=["GET", "POST"])
 def showRockets():
-    conn = sqlite3.connect('db.sqlite3')
+    conn = sqlite3.connect(dbPath)
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
     
+    if request.method == "POST":
+        for key in request.form:
+            if key.startswith('delete.'):
+                id = key.split('.')[1]
+                c.execute("DELETE FROM Rockets WHERE rid = ?", (id,))
+                conn.commit()
+                return redirect(url_for("showRockets"))
+        if request.form["thrust"]:
+            userValues = (request.form["rname"], request.form["thrust"], request.form["vendor"], request.form["fuelTankSize"], request.form["fuelBurnRate"], request.form["crewCapacity"])
+            c.execute("INSERT INTO Rockets (rname, thrust, vendor, fuelTankSize, fuelBurnRate, crewCapacity) VALUES (?, ?, ?, ?, ?, ?)", userValues)
+   
     c.execute("SELECT * FROM Rockets")
     results = c.fetchall()
-
-    return render_template("rockets.html", headers=("Rocket ID","Thrust","Vendor","Fuel Tank Size","Fuel Burn Rate"), rows=results)
+    
+    formInputs = [['rname', 'text', 'Rocket Name', []], ['thrust', 'number', 'Thrust', []], ['vendor', 'text', 'Vendor', []], ['fuelTankSize', 'number', 'Fuel Tank Size', []], ['fuelBurnRate', 'number', 'Fuel Burn Rate', []], ['crewCapacity', 'number', 'Crew Capacity', []]]
     
     conn.commit()
     conn.close()
-
+    
+    return render_template("rockets.html", headers=("Rocket ID","Rocket Name","Thrust","Vendor","Fuel Tank Size","Fuel Burn Rate", "Crew Capacity"), rows=results, inputs=formInputs)
+       
 if __name__ == "__main__":
     app.run()
         
-# conn.commit()
-# conn.close()
