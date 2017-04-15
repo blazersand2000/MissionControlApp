b28bfdc624485b53ed08976695934b1a44fc2199
Andrew <blazersand2000@users.noreply.github.com>
2017-04-15 14:41:10 -0400

    Added login system.

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, redirect, request, url_for
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 import sqlite3
 
 dbPath = 'db.sqlite3'
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/missions", methods=["GET", "POST"])
+@login_required
 def showMissions():
     #open database connection
     conn = sqlite3.connect(dbPath)
@@ -60,6 +64,7 @@ def showMissions():
     return render_template("missions.html", headers=("Rocket","Facility","Time of Launch", "Time of Landing", "Crew Members"), rows=results, inputs=formInputs)
     
 @app.route("/launch_facilities", methods=["GET", "POST"])
+@login_required
 def showFacilities():
     conn = sqlite3.connect(dbPath)
     conn.row_factory = sqlite3.Row
@@ -88,6 +93,7 @@ def showFacilities():
     
 
 @app.route("/rockets", methods=["GET", "POST"])
+@login_required
 def showRockets():
     conn = sqlite3.connect(dbPath)
     conn.row_factory = sqlite3.Row
@@ -115,6 +121,7 @@ def showRockets():
     return render_template("rockets.html", headers=("Rocket Name","Thrust","Vendor","Fuel Tank Size","Fuel Burn Rate", "Crew Capacity"), rows=results, inputs=formInputs)
        
 @app.route("/astronauts", methods=["GET", "POST"])
+@login_required
 def showAstronauts():
     conn = sqlite3.connect(dbPath)
     conn.row_factory = sqlite3.Row
@@ -140,7 +147,102 @@ def showAstronauts():
     conn.close()
     
     return render_template("astronauts.html", headers=("First Name","Last Name","Date of Birth","Hours Flown"), rows=results, inputs=formInputs)
+
+@app.route("/login", methods=["GET", "POST"])
+def showLogin():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    userNotFound=""
+    wrongPassword=""
+    if request.method == "POST":
+        userRecord = DBgetUser(request.form["username"])
+        if userRecord:
+            if request.form["password"] == userRecord["password"]:
+                user = User(userRecord["username"], userRecord["password"])
+                user.authenticated = True
+                login_user(user)
+                return redirect(url_for("index"))
+            else:
+                print("wrong password")
+                wrongPassword="Pasword is incorrect"
+        else:
+            print("user not found")
+            userNotFound="User doesn't exist"
+
+    formInputs = [['username', 'text', 'Username', []], ['password', 'password', 'Password', []]]
+    return render_template("login.html", inputs=formInputs, userNotFound=userNotFound, wrongPassword=wrongPassword)
        
+@app.route("/signup", methods=["GET", "POST"])
+def showSignup():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    UserNameAlreadyExists = ""
+    if request.method == "POST":
+        if DBgetUser(request.form["username"]):
+            UserNameAlreadyExists = "Username already exists, try again"
+        else:
+            logout_user()
+            DBaddUser(request.form["firstName"], request.form["lastName"], request.form["username"], request.form["password"])
+            DBloginUser(request.form["username"])
+            user = User(request.form["username"], request.form["password"])
+            user.authenticated = True
+            login_user(user)
+            return redirect(url_for("index"))
+
+    formInputs = [['firstName', 'text', 'First Name', []], ['lastName', 'text', 'Last Name', []], ['username', 'text', 'Username', []], ['password', 'password', 'Password', []]]
+    return render_template("signup.html", inputs=formInputs, UserNameAlreadyExists=UserNameAlreadyExists)
+
+@app.route("/logout")
+def showLogout():
+    if not current_user.is_authenticated:
+        return redirect(url_for("index"))
+    logout_user()
+    return render_template("logout.html")
+
+class User(UserMixin):
+
+    def __init__(self, username, password):
+        self.id = username
+        self.password = password
+
+@login_manager.user_loader
+def load_user(username):
+    userRecord = DBgetUser(username)
+
+    if userRecord:
+        return User(userRecord["username"], userRecord["password"])
+
+    return None
+
+def DBgetUser(username):
+    conn = sqlite3.connect(dbPath)
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+
+    c.execute("SELECT * from Users WHERE username = ?", (username,))
+    userRecord = c.fetchone()
+
+    conn.close()
+
+    return userRecord
+
+def DBaddUser(firstname, lastname, username, password):
+    conn = sqlite3.connect(dbPath)
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    c.execute("INSERT INTO Users (firstName, lastName, username, password, regTime) VALUES (?, ?, ?, ?, datetime('now'))", (firstname, lastname, username, password))
+    conn.commit()
+    conn.close()
+
+def DBloginUser(username):
+    conn = sqlite3.connect(dbPath)
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    c.execute("UPDATE Users SET lastLoginTime = datetime('now') WHERE username = ?", (username,))
+    conn.commit()
+    conn.close()
+
 if __name__ == "__main__":
+    app.config["SECRET_KEY"] = "ITSASECRET"
     app.run()
         
